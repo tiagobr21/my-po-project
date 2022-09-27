d227d21ac9799dd6c799b155ecc176d0fe135818
tiagobr21 <tiagobrr17@gmail.com>
2022-09-27 16:38:07 -0300

    selecionar todos os checkbox

--- src/app/components/novo-historico/dados-pessoais/dados-pessoais.component.ts
+++ src/app/components/novo-historico/dados-pessoais/dados-pessoais.component.ts
@@ -2,6 +2,7 @@
 import { Component, OnInit,Input,Output,EventEmitter } from '@angular/core';
 import { BackconnectService } from 'src/app/service/backconnect.service';
 import { FormBuilder,FormGroup,FormControl,FormArray,ValidatorFn, Validators } from '@angular/forms';
+import { retry } from 'rxjs';
 
 
 @Component({
@@ -22,74 +23,61 @@ export class DadosPessoaisComponent implements OnInit {
   enable4:boolean = false; 
   @Output() alunosSelecionados = new EventEmitter();
   
-  checks:boolean = false
+  checks:boolean = false;
   Diplomados:any;
   cursoSelecionado:any;
   dadosAlunos:any[]=[];
   teste:any;
-  page1:any[]=[]
-  page2:any[]=[]
-  page3:any[]=[]
-  page4:any[]=[]
-  page5:any[]=[]
-  contador:any
-  keys:any
-  page1Select:boolean=true
-  page2Select:boolean=false
-  page3Select:boolean=false
-  page4Select:boolean=false
-  page5Select:boolean=false
-  Allcheckbox:any[]=[]
+  page1:any[]=[];
+  page2:any[]=[];
+  page3:any[]=[];
+  page4:any[]=[];
+  page5:any[]=[];
+  contador:any;
+  keys:any;
+  page1Select:boolean=true;
+  page2Select:boolean=false;
+  page3Select:boolean=false;
+  page4Select:boolean=false;
+  page5Select:boolean=false;
+  checkbox:any[]=[];
+  allCheckes:boolean = true;
   
 
   constructor(private service:BackconnectService,private fb:FormBuilder) {
      this.userForm = this.fb.group({
-       checkArray: this.fb.array([],[Validators.required]),
+       checkArray: this.fb.array([],[Validators.required])
    
      })
   }
 
   onCheckboxChange(e:any){
-  const checkArray: FormArray = this.userForm.get('checkArray') as FormArray;
+    const checkArray: FormArray = this.userForm.get('checkArray') as FormArray;
+    const id = e.target.value;
+    const isChecked = e.target.checked;
 
 
-    if(e.target.checked){
-
-      console.log(e.target.value)
-   
-      checkArray.push(new FormControl(e.target.value))
-
-
-
-
-    }  else{
-
-      var i=0; 
-
-      checkArray.controls.forEach((item:any)=>{
+   this.Diplomados =  this.Diplomados.map((i:any)=>{
+      if(i.Dadosdiplomadiplomadoid === id){
+        i.Checked = isChecked
+        this.allCheckes = false;
+        checkArray.push(new FormControl(i.Dadosdiplomadiplomadoid)) 
+      
+      }
+      if(id == -1){
+        i.Checked = this.allCheckes;
+        checkArray.push(new FormControl(i.Dadosdiplomadiplomadoid)) 
+        return i;
+      }
      
-        if (item.value == e.target.value){
-      
-          checkArray.removeAt(i);
-          return;
-        }
-        i++;
-      });
-    }  
-
-  }
-
-  bulk(e:any){
-
-    if(e.target.checked){
-      this.checks = true;
-      
-    }else{
-      this.checks = false;
-    }
+      return i;
+    });
+    // console.log(this.Diplomados);
   }
   
   ngSubmit(){
+  
+ console.log(this.userForm.value);
 
 
   }
@@ -98,11 +86,16 @@ export class DadosPessoaisComponent implements OnInit {
     
     this.service.listarDiplomados().subscribe((res):any=>{
       this.Diplomados = res;
+      
+       this.Diplomados.map( (diplomado:any) => {
+        diplomado.Checked = false;
+      }) 
       this.next = true;
- 
+
+      
     })
         scrollTo(10, 0);
-    
+
   }
 
   
@@ -110,3 +103,59 @@ export class DadosPessoaisComponent implements OnInit {
    
   }
 }
+ /*  
+onCheckboxChange(e:any){
+  const checkArray: FormArray = this.userForm.get('checkArray') as FormArray;
+
+    if(e.target.checked){
+      checkArray.push(new FormControl(e.target.value))
+
+    }else{
+      var i=0; 
+
+      checkArray.controls.forEach((item:any)=>{
+      
+        if (item.value == e.target.value){
+          console.log(item.value)
+          checkArray.removeAt(i);
+          return;
+        }
+        i++;
+      });
+   }
+   console.log(checkArray.value)
+  }
+
+  bulk(e:any){
+    this.checks = true;
+    const allCheckArray: FormArray = this.userForm.get('checkArray') as FormArray;
+    
+    if(e.target.checked){
+      
+      allCheckArray.push(new FormControl(e.target.value))
+
+      allCheckArray.controls.forEach((item:any,i:any)=>{
+    
+          console.log(item.value)
+          return item.value
+
+
+      });
+    }else{
+      this.checks = false;
+      var i=0; 
+      
+      allCheckArray.controls.forEach((item:any)=>{
+      
+        if (item.value == e.target.value){
+          console.log(item.value)
+    
+          allCheckArray.removeAt(i);
+          return;
+        }
+        i++;
+      });
+    }
+    
+
+  }*/
